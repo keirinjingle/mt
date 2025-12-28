f8d06c330cb48882c32101f35157850b1215acfe
keirinjingle <keirinjingle@gmail.com>
2025-12-28 23:18:41 +0000

    Init Vite React app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>もふタイマー</h1>
+      </header>
+
+      <main>
+        <p>フロント初期化完了 🐾</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
